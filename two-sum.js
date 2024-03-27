2f16c2810c8c452fa0d7c47a85ce50a1c44c08f2
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2024-03-27 12:06:19 +0200

    fix two-sum

--- two-sum.js
+++ two-sum.js
@@ -4,18 +4,8 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  // const map = new Map();
-  // for (let i = 0; i < nums.length; i++) {
-  //   const complement = target - nums[i];
-  //   if (map.has(complement)) {
-  //     return [map.get(complement), i];
-  //   }
-  //   map.set(nums[i], i);
-  // }
-  // return [];
-
   const map = new Map();
-  for (let i = 0; i > nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
     if (map.has(complement)) {
       return [map.get(complement), i];
