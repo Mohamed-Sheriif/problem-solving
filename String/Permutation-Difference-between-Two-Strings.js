c945c1735b5813149926184f1fe16fd34c8f59f1
mosherif <mohamedsh2782@gmail.com>
2024-08-29 09:54:15 +0300

    solve string problems

---
+++ String/Permutation-Difference-between-Two-Strings.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var findPermutationDifference = function (s, t) {
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    res += Math.abs(i - t.indexOf(s[i]));
+  }
+
+  return res;
+};
+
+console.log(findPermutationDifference("abcde", "edbac"));
